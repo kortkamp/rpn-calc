3ef95c98ec1ba9ffcdf19678e2339a43563b257d
kortkamp <marcelusmedius@gmail.com>
2021-07-01 07:56:40 -0300

    feat: implement UI

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,5 +1,7 @@
 import { DigitKeyboard } from "./DigitKeyboard"
+import { Key } from "./Key"
 
+import '../styles/keyboard.scss'
 
 export function Keyboard(){
 
@@ -7,15 +9,33 @@ export function Keyboard(){
     console.log(keyName)
   }
 
+  function handleOperation(){
+
+  }
 
   
 
   return(
     <div className= 'keyboard-calc'>
-      <div className= 'operation-keyboard'>
       
+      <div className='operation-keyboard'>
+        <Key label='+' keyFunction={handleOperation} />
+        <Key label='-' keyFunction={handleOperation} />
+        <Key label='x' keyFunction={handleOperation} />
+        <Key label='/' keyFunction={handleOperation} />
       </div>
+      
       <DigitKeyboard handleDigit = {handleDigit} />
+
+
+      <div className='data-keyboard'>
+        <Key label='C' keyFunction={handleOperation} />
+        <Key label='E' keyFunction={handleOperation} />
+        
+        
+        
+      </div>
+      
     </div>
   )
 }
---
+++ src/components/OperationKeyboard.tsx
@@ -0,0 +1,6 @@
+export function OperationKeyboard(){
+
+  return(
+    <div></div>
+  )
+}
